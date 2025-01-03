7810d2579e7047bf00bff2b8696b74f050b76e23
User <user@codeinstitute.net>
2025-01-03 12:59:10 +0100

    Pre-deployment commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$x7o&rbu#6=fk&797lu+7&f(3$trs+)+lyg8b)r97eas_j6zcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
